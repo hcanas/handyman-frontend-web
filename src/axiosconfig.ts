8a967bc8ed9d2378e537d32751735ad82ab2e497
hcanas <harveyanas@gmail.com>
2025-08-10 16:03:48 +0800

    intercept 401 api response and redirect to login page

--- src/axiosconfig.ts
+++ src/axiosconfig.ts
@@ -1,4 +1,22 @@
 import axios from "axios";
+import {useAuthStore} from "./stores/auth-store.ts";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      useAuthStore().user = {
+        name: '',
+        email: '',
+        role: null,
+      };
+
+      window.location.href = '/login';
+    }
+
+    return Promise.reject(error);
+  }
+);

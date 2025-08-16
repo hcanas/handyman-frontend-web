3e6e74b44f0e8949b0cb0672a5cf22655f4eb713
hcanas <harveyanas@gmail.com>
2025-08-16 15:57:31 +0800

    add routes for placeholders for ticket overview and new ticket pages

--- src/routes/ticket-routes.ts
+++ src/routes/ticket-routes.ts
@@ -7,6 +7,18 @@ const ticketRoutes: RouteRecordRaw[] = [
     component: () => import("../views/tickets/Tickets.vue"),
     meta: { requiresAuth: true },
   },
+  {
+    path: "/tickets/new",
+    name: "new-ticket",
+    component: () => import("../views/tickets/NewTicket.vue"),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/ticket/:id/overview",
+    name: "ticket-overview",
+    component: () => import("../views/tickets/overview/TicketOverview.vue"),
+    meta: { requiresAuth: true },
+  },
 ];
 
 export default ticketRoutes;

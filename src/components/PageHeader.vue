934b0f93bee748de8db392ff0ec54fe4b239126e
hcanas <harveyanas@gmail.com>
2025-08-16 15:56:55 +0800

    add actions slot for page header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -5,5 +5,8 @@ document.title = props.title || "Handyman";
 </script>
 
 <template>
-  <h1 class="text-3xl text-neutral-100 font-medium tracking-wide">{{ title }}</h1>
+  <div class="flex items-center justify-between gap-x-6">
+    <h1 class="text-3xl text-neutral-100 font-medium tracking-wide">{{ title }}</h1>
+    <slot name="actions"/>
+  </div>
 </template>
